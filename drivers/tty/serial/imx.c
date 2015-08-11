d09bdea918da2891bdfa0f4eac994d6a3fce5cb6
Yuma Arakawa <yuma.arakawa@atmark-techno.com>
2015-08-11 14:23:01 +0900

    serial: imx: Add warning when dropping the data to insert into the flip buffer
    flipバッファへのデーター追加(tty_insert_flip_string_flags関数)後、追加
    されたデーターサイズが引数で指定したデーターサイズより小さい場合は警告
    メッセージを出力するよう修正する。
    
    imx_rxint関数内では、tty_insert_flip_string_flags関数実行前に
    tty_buffer_request_room関数でflipバッファの空きを確認しているため、条
    件にunlikelyを指定している。
    
    Reviewed-by: Daisuke Mizobuchi <mizo@atmark-techno.com>
    Reviewed-by: Yasushi SHOJI <yashi@atmark-techno.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -774,6 +774,7 @@ static irqreturn_t imx_rxint(int irq, void *dev_id)
 	unsigned char chars_buf[sport->port.fifosize];
 	char flags_buf[sport->port.fifosize];
 	unsigned int rx_count = 0;
+	int inserted;
 
 	room = tty_buffer_request_room(port, sport->port.fifosize);
 
@@ -854,11 +855,14 @@ static irqreturn_t imx_rxint(int irq, void *dev_id)
 
 out:
 	if (likely(rx_count)) {
-		if (tty_insert_flip_string_flags(port, chars_buf,
-						 flags_buf, rx_count) < rx_count)
+		inserted = tty_insert_flip_string_flags(port, chars_buf,
+							flags_buf, rx_count);
+		if (unlikely(inserted < rx_count)) {
+			dev_warn(sport->port.dev, "%s - dropping data, %d bytes lost\n",
+				 __func__, rx_count - inserted);
 			sport->port.icount.buf_overrun++;
+		}
 	}
-
 	spin_unlock_irqrestore(&sport->port.lock, flags);
 	tty_flip_buffer_push(port);
 	return IRQ_HANDLED;
