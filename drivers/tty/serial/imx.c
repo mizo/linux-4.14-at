671a44eefcf30bb72168a12aa74a61b9ae63739f
Daisuke Mizobuchi <mizo@atmark-techno.com>
2018-02-01 18:55:30 +0900

    serial: imx: do RS485 config when RS485 enable at boot time

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2240,6 +2240,7 @@ static int serial_imx_probe_dt(struct imx_port *sport,
 		struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
+	struct serial_rs485 *rs485conf = &sport->port.rs485;
 	int ret;
 
 	sport->devdata = of_device_get_match_data(&pdev->dev);
@@ -2260,6 +2261,18 @@ static int serial_imx_probe_dt(struct imx_port *sport,
 
 	if (of_get_property(np, "fsl,dte-mode", NULL))
 		sport->dte_mode = 1;
+
+	if (of_property_read_bool(np, "rs485-enabled-at-boot-time")) {
+		rs485conf->flags |= SER_RS485_ENABLED;
+
+		if (of_property_read_bool(np, "rs485-rx-during-tx"))
+			rs485conf->flags |= SER_RS485_RX_DURING_TX;
+
+		if (of_property_read_bool(np, "rs485-rts-active-high"))
+			rs485conf->flags |= SER_RS485_RTS_ON_SEND;
+		else
+			rs485conf->flags |= SER_RS485_RTS_AFTER_SEND;
+	}
 
 	sport->rs485_duplex_gpio = of_get_named_gpio(np, "fsl,rs485-duplex-gpio", 0);
 	if (gpio_is_valid(sport->rs485_duplex_gpio)) {
@@ -2336,8 +2349,10 @@ static int serial_imx_probe(struct platform_device *pdev)
 	sport->port.fifosize = 32;
 	sport->port.ops = &imx_pops;
 	sport->port.rs485_config = imx_rs485_config;
-	sport->port.rs485.flags =
-		SER_RS485_RTS_ON_SEND | SER_RS485_RX_DURING_TX;
+	if (!(sport->port.rs485.flags & SER_RS485_ENABLED)) {
+		sport->port.rs485.flags =
+			SER_RS485_RTS_ON_SEND | SER_RS485_RX_DURING_TX;
+	}
 	sport->port.flags = UPF_BOOT_AUTOCONF;
 	init_timer(&sport->timer);
 	sport->timer.function = imx_timeout;
