97c997dc003dfc2f664d22f0e08100880cc8f3d6
Daisuke Mizobuchi <mizo@atmark-techno.com>
2018-02-14 09:44:38 +0900

    serial: imx: ignore set_mctrl in RS485 mode

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -1050,7 +1050,8 @@ static void imx_set_mctrl(struct uart_port *port, unsigned int mctrl)
 		temp |= UTS_LOOP;
 	writel(temp, sport->port.membase + uts_reg(sport));
 
-	mctrl_gpio_set(sport->gpios, mctrl);
+	if (!(port->rs485.flags & SER_RS485_ENABLED))
+		mctrl_gpio_set(sport->gpios, mctrl);
 }
 
 /*
