06e2a1824e6779550b106abe1524c67e7351b25f
Daisuke Mizobuchi <mizo@atmark-techno.com>
2019-06-24 09:22:41 +0900

    for JIG: fix vbus-sel

--- arch/arm/mach-imx/vbus_sel-imx6.c
+++ arch/arm/mach-imx/vbus_sel-imx6.c
@@ -7,62 +7,9 @@
 #include <linux/regulator/consumer.h>
 #include "common.h"
 
-static struct notifier_block nb_otg1, nb_otg2;
-
-static int imx6_vbus_sel_regulator_notify(struct notifier_block *nb,
-					  unsigned long event,
-					  void *ignored)
-{
-	if (event == REGULATOR_EVENT_PRE_DISABLE) {
-		/*1:USB_OTG1_VBUS, 0:USB_OTG2_VBUS*/
-		imx_anatop_3p0_vbus_sel(&nb_otg1 != nb);
-	}
-
-	return NOTIFY_OK;
-}
-
-
 static int imx6_vbus_sel_probe(struct platform_device *pdev)
 {
-	struct regulator *otg1_vbus_reg, *otg2_vbus_reg;
-	unsigned long flags;
-	int ret;
-
-	otg1_vbus_reg = devm_regulator_get_optional(&pdev->dev, "otg1-vbus-reg");
-	if (IS_ERR(otg1_vbus_reg)) {
-		if (PTR_ERR(otg1_vbus_reg) == -EPROBE_DEFER)
-			return -EPROBE_DEFER;
-	} else {
-		nb_otg1.notifier_call = &imx6_vbus_sel_regulator_notify;
-		ret = devm_regulator_register_notifier(otg1_vbus_reg, &nb_otg1);
-		if (ret) {
-			dev_err(&pdev->dev,
-				"OTG1 VBUS regulator notifier request failed\n");
-			return ret;
-		}
-
-		local_irq_save(flags);
-		imx_anatop_3p0_vbus_sel(regulator_is_enabled(otg1_vbus_reg));
-		local_irq_restore(flags);
-	}
-
-	otg2_vbus_reg = devm_regulator_get_optional(&pdev->dev, "otg2-vbus-reg");
-	if (IS_ERR(otg2_vbus_reg)) {
-		if (PTR_ERR(otg2_vbus_reg) == -EPROBE_DEFER)
-			return -EPROBE_DEFER;
-	} else {
-		nb_otg2.notifier_call = &imx6_vbus_sel_regulator_notify;
-		ret = devm_regulator_register_notifier(otg2_vbus_reg, &nb_otg2);
-		if (ret) {
-			dev_err(&pdev->dev,
-				"OTG2 VBUS regulator notifier request failed\n");
-			return ret;
-		}
-
-		local_irq_save(flags);
-		imx_anatop_3p0_vbus_sel(!regulator_is_enabled(otg2_vbus_reg));
-		local_irq_restore(flags);
-	}
+	imx_anatop_3p0_vbus_sel(1);
 
 	return 0;
 }
